a5d9c98eea43de0ac428662371360b2f00a634da
FishYoung <youngfish93@hotmail.com>
2015-06-12 13:47:34 +0800

    test
    add a temp.cpp to test github

---
+++ temp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bitset>
+using namespace std;
+int main()
+{
+    int a ;
+    a = 10;
+    bitset<8>bit_a;
+    bit_a = a;
+    cout<<bit_a<<endl;
+    cout<<"hello,world"<<endl;
+    return 0;
+}
